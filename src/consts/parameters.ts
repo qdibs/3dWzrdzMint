b912babbd7d90c2d5fc1a556f08ae4a919bdeed6
Qdibs <72762489+qdibs@users.noreply.github.com>
2023-11-22 11:18:07 -0500

    Add files via upload

---
+++ src/consts/parameters.ts
@@ -0,0 +1,26 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "0xc0D7A22106AB9d26f807b1c58680c42b986e7a4A";
+
+// The name of the chain your contract is deployed to.
+// Refer to README.md on how to specify the chain name.
+export const chainConst = "ethereum";
+
+// It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
+// Please ensure that you define the correct domain for your API key from the API settings page.
+// You can get one for free at https://thirdweb.com/create-api-key
+// Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
+export const clientIdConst = import.meta.env.VITE_TEMPLATE_CLIENT_ID || "a5b72237dfef5d482d6dac8b4435de26";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "blue";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
